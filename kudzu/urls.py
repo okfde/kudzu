e69fd846278713b7a2880dad62e605988d8bd0fa
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2014-06-12 15:02:37 +0200

    Add admin, static files, templates, heroku files

--- kudzu/urls.py
+++ kudzu/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'kudzu.views.home', name='home'),
+    url(r'^$', 'kudzu.seeds.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
